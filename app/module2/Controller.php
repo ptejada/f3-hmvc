b65712d0bef4d3e9d89df5b109a67dcd84d508b7
Konstantinos Bariotis <konmpar@gmail.com>
2013-07-31 19:20:25 +0300

    eliminate Template echoing on each action

--- app/module2/Controller.php
+++ app/module2/Controller.php
@@ -8,16 +8,9 @@ class Controller extends BaseController {
 	function index($app){
 		//Model from module1
 		$app->set("data", new \module1\models\Data());
-		echo Template::instance()->render("default.html");
 	}
 
-	function page1($app){
-		echo Template::instance()->render("page1.html");
+	function page1($app){}
 
-	}
-
-	function page2($app){
-		echo Template::instance()->render("page2.html");
-
-	}
+	function page2($app){}
 }
